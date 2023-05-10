ec1a871625d5c764e2a94a50a574409087936e11
ImAHappyDuck <bosscj21@gcc.edu>
2023-05-09 21:19:32 -0400

    started single player class

---
+++ src/SinglePlayerGameLoop.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class SinglePlayerGameLoop {
+    Board b = new Board();
+    boolean hasWon = false;
+
+
+
+    public void gameLoop ()
+    {
+        System.out.println("The rules are simple: \n every time you miss, two new enemy cells appear in the board. \n" +
+                "Kill them all before the board fills up.");
+        while (!hasWon)
+        {
+Random r = new Random();
+
+
+
+
+
+
+        }
+    }
+}
