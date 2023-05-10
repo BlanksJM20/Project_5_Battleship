78ad5088524f9580247cae0d413d9d3450b8549c
ImAHappyDuck <bosscj21@gcc.edu>
2023-05-09 21:01:07 -0400

    deleted comp player class

--- src/ComputerPlayer.java
+++
@@ -1,9 +0,0 @@
-public class ComputerPlayer extends Player{
-    private Board compBoard;
-    //default constructor - won't be used for actual play
-    public ComputerPlayer (Board b)
-    {
-        super(b);
-        //
-    }
-}
